b3f23d207608830774913bde7563687fb1fe383e
Failed Save <failedsavevspetrify@gmail.com>
2020-01-23 22:20:04 -0500

    Created a Party class and moved the list of characters  into that

---
+++ PerilDungeon/Classes/Party.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerilDungeon.Classes
+{
+    public class Party
+    {
+        public List<Character> PartyMembers { get; set; }
+        Random rng;
+
+        public Party()
+        {
+            PartyMembers = new List<Character>();
+            rng = new Random();
+        }
+
+        public Character GetRandomCharacter()
+        {
+            return PartyMembers[rng.Next(0, PartyMembers.Count)];
+        }
+
+        public bool HasCharacterActive
+        {
+            get
+            { //TODO
+                return true;
+            }
+        }
+    }
+}
