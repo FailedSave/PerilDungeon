07509f743a115d309975e26d51e6fa9ea6ba9675
Failed Save <failedsavevspetrify@gmail.com>
2020-02-11 01:17:42 -0500

    feat: Encounter picker can now choose from all available encounters
    Resolves #32

--- PerilDungeon/Classes/EncounterSelector.cs
+++ PerilDungeon/Classes/EncounterSelector.cs
@@ -37,23 +37,42 @@ namespace PerilDungeon.Classes
 
             IEncounter encounter;
 
-            if (rng.NextDouble() > 0.8)
-            {
-                encounter = new StairsEncounter();
-            }
-            else
-            {
-                if (party.Depth > 3)
-                {
-                    encounter = new UnweaverEncounter();
-                }
-                else
-                {
-                    encounter = new FountainEncounter();
-                }
-            }
+            encounter = (IEncounter)Activator.CreateInstance(pickEncounterFromWeightedList(generateEncounterTable(party)));
+
             encounter.Party = party;
             return encounter;
+        }
+
+        private static Dictionary<Type, double> generateEncounterTable(Party party)
+        {
+            var table = new Dictionary<Type, double>();
+            table.Add(typeof(StairsEncounter), 10.0);
+            table.Add(typeof(BasiliskEncounter), 10.0);
+            table.Add(typeof(TrappedDoorEncounter), 10.0);
+            table.Add(typeof(BadAirEncounter), 10.0);
+            table.Add(typeof(FountainEncounter), 10.0);
+            if (party.Depth > 3)
+            {
+                table.Add(typeof(UnweaverEncounter), 20.0);
+            }
+            return table;
+        }
+
+        private static Type pickEncounterFromWeightedList(Dictionary<Type, double> table)
+        {
+            double total = table.Values.Sum();
+            double target = rng.NextDouble() * total;
+            double currentIndex = 0;
+
+            foreach (var entry in table)
+            {
+                currentIndex += entry.Value;
+                if (currentIndex >= target)
+                {
+                    return entry.Key;
+                }
+            }
+            return typeof(BasicEncounter);
         }
     }
 }
