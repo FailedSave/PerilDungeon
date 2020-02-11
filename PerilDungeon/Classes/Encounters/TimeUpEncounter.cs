7df30b416576db06d6f3b70ac823cc3d0586177d
Failed Save <failedsavevspetrify@gmail.com>
2020-02-11 00:46:17 -0500

    feat: time can now run out
    Resolves #13

---
+++ PerilDungeon/Classes/Encounters/TimeUpEncounter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerilDungeon.Classes.Encounters
+{
+    public class TimeUpEncounter : IEncounter
+    {
+        public string Title { get => "Time Has Run Out"; set { } }
+        public string Description { get => "The three days you were given have run out. The guards abandon you for lost and reseal the entrance. You carry on as best you can, but are eventually overwhelmed by the dangers of the Delve."; set { } }
+        public Party Party { get; set; }
+        public IEnumerable<IEncounterChoice> Choices
+        {
+            get
+            {
+                List<IEncounterChoice> choices = new List<IEncounterChoice>();
+                choices.Add(new TimeUpEncounterChoice());
+                return choices;
+            }
+            set { }
+        }
+    }
+}
---
+++ PerilDungeon/Classes/Encounters/TimeUpEncounterChoice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerilDungeon.Classes.Encounters
+{
+    public class TimeUpEncounterChoice : IEncounterChoice
+    {
+        public string Text { get => "Despair"; set { } }
+        public Func<Party, IEnumerable<string>> Choose
+        {
+            get
+            {
+                return (Party party) =>
+                {
+                    List<string> messages = new List<string>();
+
+                    messages.Add("Your party is overwhelmed by the perils surrounding every turn, and eventually finds themselves transformed into statues.");
+
+                    foreach (Character c in party.PartyMembers)
+                    {
+                        c.AddStatus("Petrified");
+                    }
+
+                    return messages;
+                };
+            }
+            set { }
+        }
+
+        public bool IsAvailable { get { return true; } set { } }
+
+        public IEncounter GetNextEncounter(Party p, IEncounter encounter)
+        {
+            return EncounterSelector.PickEncounter(p, null);
+        }
+    }
+}
