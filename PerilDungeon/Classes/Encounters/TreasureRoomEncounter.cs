2c4a04afdba93563b6fb83eb001e054e4468cba5
Failed Save <failedsavevspetrify@gmail.com>
2020-02-11 21:07:40 -0500

    feat: trapped doors now hide treasure
    Resolves #35

---
+++ PerilDungeon/Classes/Encounters/TreasureRoomEncounter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerilDungeon.Classes.Encounters
+{
+    public class TreasureRoomEncounter : IEncounter
+    {
+        public string Title { get => "Treasure Room"; set { } }
+        public string Description { get => "Sure enough, the trap guarded a small chamber with treasure and valuable items!"; set { } }
+        public Party Party { get; set; }
+        public IEnumerable<IEncounterChoice> Choices
+        {
+            get
+            {
+                List<IEncounterChoice> choices = new List<IEncounterChoice>();
+                choices.Add(new TreasureRoomEncounterChoice());
+                return choices;
+            }
+            set { }
+        }
+    }
+}
---
+++ PerilDungeon/Classes/Encounters/TreasureRoomEncounterChoice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerilDungeon.Classes.Encounters
+{
+    public class TreasureRoomEncounterChoice : IEncounterChoice
+    {
+        public string Text { get => "Score!"; set { } }
+        public Func<Party, IEnumerable<string>> Choose
+        {
+            get
+            {
+                return (Party party) =>
+                {
+                    List<string> messages = new List<string>();
+
+                    messages.Add("You add the loot to your bags and move on.");
+
+                    party.Money += party.Depth * EncounterSelector.rng.Next(20, 40) + EncounterSelector.rng.Next(1, 5);
+
+                    return messages;
+                };
+            }
+            set { }
+        }
+
+        public bool IsAvailable { get { return true; } set { } }
+
+        public IEncounter GetNextEncounter(Party p, IEncounter encounter)
+        {
+            return EncounterSelector.PickEncounter(p, typeof(ExplorationEncounter));
+        }
+    }
+}
