ab7302866a1637646ebe29c6d8897c69b7b2d71f
Failed Save <failedsavevspetrify@gmail.com>
2020-02-12 23:11:56 -0500

    feat: add gelatious cube encounter

---
+++ PerilDungeon/Classes/Encounters/GelatinousCubeEncounter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerilDungeon.Classes.Encounters
+{
+    public class GelatinousCubeEncounter : IEncounter
+    {
+        public string Title { get => "Acidic Cube"; set { } }
+        public string Description { get => "You hear it before you spot it: a slurping sound of a transparent acidic jelly, sliding through the corridors, is almost upon you. If you turn and sprint away, you can probably escape. On the other hand, you can take out your weapons and try to defeat it."; set { } }
+        public Party Party { get; set; }
+        public IEnumerable<IEncounterChoice> Choices
+        {
+            get
+            {
+                List<IEncounterChoice> choices = new List<IEncounterChoice>();
+                choices.Add(new FightGelatinousCubeEncounterChoice());
+                choices.Add(new FleeGelatinousCubeEncounterChoice());
+                return choices;
+            }
+            set { }
+        }
+    }
+}
